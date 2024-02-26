a252d261558be37ba58c82776cbf33acb625cc06
Ryan-Sin <ryansin937@gmail.com>
2024-02-27 00:00:37 +0900

    fix: 메모리 DB 연동으로 변경

--- src/databases/mysql.providers.ts
+++ src/databases/mysql.providers.ts
@@ -5,20 +5,20 @@ export const MysqlProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const AppDataSource = new DataSource({
-        // type: 'sqlite',
-        // database: ":memory:",
+        type: 'sqlite',
+        database: ":memory:",
+        entities: ['dist/**/*.entity.{ts,js}'],
+        logging: process.env.NODE_ENV === 'local' ? true : false,
+        synchronize: true
+        // type: 'mysql',
+        // host: process.env.MYSQL_MASTER_HOST,
+        // port: parseInt(process.env.MYSQL_MASTER_PORT),
+        // username: process.env.MYSQL_MASTER_USER,
+        // password: process.env.MYSQL_MASTER_PASSWORD,
+        // database: process.env.MYSQL_MASTER_DATABASE,
+        // bigNumberStrings: false,
         // entities: ['dist/**/*.entity.{ts,js}'], // Entity 연결
         // logging: process.env.NODE_ENV === 'local' ? true : false,
-        // synchronize: true
-        type: 'mysql',
-        host: process.env.MYSQL_MASTER_HOST,
-        port: parseInt(process.env.MYSQL_MASTER_PORT),
-        username: process.env.MYSQL_MASTER_USER,
-        password: process.env.MYSQL_MASTER_PASSWORD,
-        database: process.env.MYSQL_MASTER_DATABASE,
-        bigNumberStrings: false,
-        entities: ['dist/**/*.entity.{ts,js}'], // Entity 연결
-        logging: process.env.NODE_ENV === 'local' ? true : false,
         // synchronize: true
       });
 
